ed35081aca879854e261587dc9360a6549c3fc67
Diounata <jonathamcordeiro42@gmail.com>
2022-05-27 23:20:20 -0300

    add: header bold font

--- client/src/components/globalStyle.ts
+++ client/src/components/globalStyle.ts
@@ -8,6 +8,10 @@ const GlobalStyle = createGlobalStyle`
     
     font: ${({ theme }) => theme.FONT.REGULAR};
   }
+
+  h1, h2, h3, h4, h5, h6 {
+    font: ${({ theme }) => theme.FONT.BOLD};
+  }
 `;
 
 export { GlobalStyle };
