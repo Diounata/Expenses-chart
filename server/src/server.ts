d443ca931504da4168dc7b9e2da952e2d3963621
Diounata <jonathamcordeiro42@gmail.com>
2022-06-06 22:29:01 -0300

    add: cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import express, { json } from 'express';
+import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
 
 app.use(json());
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(routes);
 
 app.listen(3001, () => console.log('Server is running!'));
